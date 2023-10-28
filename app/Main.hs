5c8bb6478c4e4c6da3fe1247f49f5668d43ccab0
KrunchyKrisp <m.damidavicius@gmail.com>
2023-10-28 15:02:37 +0300

    1. Added Rectangle-Circle distance

--- app/Main.hs
+++ app/Main.hs
@@ -9,9 +9,13 @@ data Shape = Circle Float Float Float | Rectangle Float Float Float Float
     deriving (Show, Ord, Eq)
 
 overlaps :: Shape -> Shape -> Bool
-overlaps (Circle x1 y1 r1) (Circle x2 y2 r2) = sqrt ((x1 - x2) ^ 2 + (y1 - y2) ^ 2) < r1 + r2
+overlaps (Circle x1 y1 r1) (Circle x2 y2 r2) = (x1 - x2) ^ 2 + (y1 - y2) ^ 2 < (r1 + r2)^2
 overlaps (Rectangle x1 y1 a1 b1) (Rectangle x2 y2 a2 b2) = x1 + a1 >= x2 && x1 <= x2 + a2 && y1 + b1 >= y2 && y1 <= y2 + b2
-
+overlaps (Rectangle x2 y2 a2 b2) (Circle x1 y1 r1) = overlaps (Circle x1 y1 r1) (Rectangle x2 y2 a2 b2) --see below
+overlaps (Circle x1 y1 r1) (Rectangle x2 y2 a2 b2) = (x1 - closestX)^2 + (y1 - closestY)^2 < r1^2
+    where --closest (x,y) of the rectangle to center of the circle
+        closestX = min (x2+a2) (max x1 x2)
+        closestY = min (y2+b2) (max y1 y2)
 -- 2
 any1, any2 :: (a -> Bool) -> [a] -> Bool
 any1 f xs = length (filter f xs) > 0
