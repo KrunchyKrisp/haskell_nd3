6dacce5d3eafefc3fdfb7464c0162eb7c92c493f
KrunchyKrisp <m.damidavicius@gmail.com>
2023-10-30 13:42:58 +0200

    6. added 2nd ver

--- app/Main.hs
+++ app/Main.hs
@@ -9,13 +9,15 @@ data Shape = Circle Float Float Float | Rectangle Float Float Float Float
     deriving (Show, Ord, Eq)
 
 overlaps :: Shape -> Shape -> Bool
-overlaps (Circle x1 y1 r1) (Circle x2 y2 r2) = (x1 - x2) ^ 2 + (y1 - y2) ^ 2 < (r1 + r2)^2
+overlaps (Circle x1 y1 r1) (Circle x2 y2 r2) = (x1 - x2) ^ 2 + (y1 - y2) ^ 2 < (r1 + r2) ^ 2
 overlaps (Rectangle x1 y1 a1 b1) (Rectangle x2 y2 a2 b2) = x1 + a1 >= x2 && x1 <= x2 + a2 && y1 + b1 >= y2 && y1 <= y2 + b2
-overlaps (Rectangle x2 y2 a2 b2) (Circle x1 y1 r1) = overlaps (Circle x1 y1 r1) (Rectangle x2 y2 a2 b2) --see below
-overlaps (Circle x1 y1 r1) (Rectangle x2 y2 a2 b2) = (x1 - closestX)^2 + (y1 - closestY)^2 < r1^2
-    where --closest (x,y) of the rectangle to center of the circle
-        closestX = min (x2+a2) (max x1 x2)
-        closestY = min (y2+b2) (max y1 y2)
+overlaps (Rectangle x2 y2 a2 b2) (Circle x1 y1 r1) = overlaps (Circle x1 y1 r1) (Rectangle x2 y2 a2 b2) -- see below
+overlaps (Circle x1 y1 r1) (Rectangle x2 y2 a2 b2) = (x1 - closestX) ^ 2 + (y1 - closestY) ^ 2 < r1 ^ 2
+  where
+    -- closest (x,y) of the rectangle to center of the circle
+    closestX = min (x2 + a2) (max x1 x2)
+    closestY = min (y2 + b2) (max y1 y2)
+
 -- 2
 any1, any2 :: (a -> Bool) -> [a] -> Bool
 any1 f xs = length (filter f xs) > 0
@@ -41,8 +43,9 @@ ff :: Integer -> [Integer] -> Integer
 ff x = last . filter (<= x) . scanl (+) 0 . map (10 *) . filter (>= 0)
 
 -- 6
-total :: (Integer -> Integer) -> Integer -> Integer
-total f n = foldr ((+) . f) 0 [0 .. n]
+total1, total2 :: (Integer -> Integer) -> Integer -> Integer
+total1 f n = (sum . map f) [0 .. n]
+total2 f n = foldr ((+) . f) 0 [0 .. n]
 
 -- 7
 iter1, iter2 :: Int -> (a -> a) -> (a -> a)
